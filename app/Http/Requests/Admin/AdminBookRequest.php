c71a918eacc43d65dd89c20813184e0342149169
Theo <theo.meunier41@gmail.com>
2022-12-31 19:06:36 +0100

    feat: create request

---
+++ app/Http/Requests/Admin/AdminBookRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AdminBookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'        => 'required|min:3',
+            'image'       => 'required',
+            'description' => 'required|min:10',
+        ];
+    }
+}
