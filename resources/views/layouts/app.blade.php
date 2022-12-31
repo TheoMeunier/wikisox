a285060215cbfb8615524ea493b1329a7d690d02
Theo <theo.meunier41@gmail.com>
2022-12-31 21:02:31 +0100

    refacto: name directory

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,14 +21,14 @@
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
+            <!-- page Heading -->
             <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
             </header>
 
-            <!-- Page Content -->
+            <!-- page Content -->
             <main>
                 {{ $slot }}
             </main>
