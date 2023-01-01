577f0952b69ae5f62c3ce4b5391713694305b34b
Isox <theo.meunier41@gmail.com>
2023-01-01 16:52:14 +0100

    ide helper and clear code

--- .scripts/ide-helper.sh
+++ .scripts/ide-helper.sh
@@ -4,7 +4,7 @@ ide_helper() {
     php artisan ide-helper:generate -H
     php artisan ide-helper:models -M
     php artisan ide-helper:meta
-}
+    }
 
 if grep -Fxq "APP_ENV=dev" .env; then
    ide_helper
