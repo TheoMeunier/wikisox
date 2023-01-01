f226c9d728117ff1450540e493caecab34b85001
Isox <theo.meunier41@gmail.com>
2023-01-01 17:36:48 +0100

    fix: clear code

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,6 @@
 import './bootstrap';
 import './libs/index'
 
-import Alpine from 'alpinejs';
-window.Alpine = Alpine;
-Alpine.start();
-
 import {createApp} from "vue";
 import Books from "./components/books.vue";
 
