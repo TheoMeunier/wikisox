8138eb0c71cff392ea0c159133e622a167c546bf
Theo <theo.meunier41@gmail.com>
2023-01-14 15:07:54 +0100

    feat: add helper.php

---
+++ app/Support/Helpers.php
@@ -0,0 +1,64 @@
+<?php
+
+if (! function_exists('numberFormat')) {
+    /**
+     * @param float $number
+     * @return string
+     */
+    function numberFormat(float $number): string
+    {
+        return number_format($number, 0, ',', ' ');
+    }
+}
+
+if (! function_exists('recursive_array_replace')) {
+    /**
+     * @param string $find
+     * @param string $replace
+     * @param $array
+     * @param bool $inKey
+     * @return array|string
+     */
+    function recursive_array_replace(string $find, string $replace, $array, bool $inKey = false): array|string
+    {
+        if (! is_array($array)) {
+            if ($inKey) {
+                return $array;
+            }
+
+            return str_replace($find, $replace, $array);
+        }
+
+        $newArray = [];
+
+        foreach ($array as $key => $value) {
+            if (
+                $inKey &&
+                'integer' !== gettype($key)
+            ) {
+                /** @var string $key */
+                $key = $key;
+
+                $key = str_replace($find, $replace, $key);
+            }
+
+            $newArray[$key] = recursive_array_replace($find, $replace, $value, $inKey);
+        }
+
+        return $newArray;
+    }
+}
+
+if (! function_exists('pathinfo_dirname')) {
+
+    /**
+     * @param array $info
+     * @return string|null
+     */
+    function pathinfo_dirname(array $info): ?string
+    {
+        $dirname = $info['dirname'] ?? null;
+
+        return $dirname === '.' ? null : $dirname;
+    }
+}
