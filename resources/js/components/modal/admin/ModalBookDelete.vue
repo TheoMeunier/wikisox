f396544ae39acc644555574f87d52edc2efd3b6b
Theo <theo.meunier41@gmail.com>
2023-01-15 13:38:31 +0100

    feat admin: books crud

---
+++ resources/js/components/modal/admin/ModalBookDelete.vue
@@ -0,0 +1,26 @@
+<template>
+  <Modal>
+    <template v-slot:button_open>
+      <button>
+        <i class="fa-solid fa-trash-can"></i>
+      </button>
+    </template>
+
+    <template v-slot:title> Supprimer le livre {{ book.name }} </template>
+
+    <template v-slot:content> Voulez vous vraiment supprimer ce page ? </template>
+
+    <template v-slot:button_action>
+      <button class="btn__danger"
+      >Supprimer
+      </button>
+    </template>
+  </Modal>
+</template>
+
+<script setup>
+import Modal from '../../modules/modal'
+const props = defineProps({
+  book: {},
+})
+</script>
