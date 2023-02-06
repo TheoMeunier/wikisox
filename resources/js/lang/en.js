0c1a080f590c835bc4f671b238eb1b628b1697e8
tmeunier <theo.meunier41@gmail.com>
2023-02-06 23:12:20 +0100

    lang to en

--- resources/js/lang/en.js
+++ resources/js/lang/en.js
@@ -1,3 +1,47 @@
 export const data = {
-    "hello": "Hello",
+    "search": "Search",
+    "table": {
+        "id": "ID",
+        "username": "User",
+        "name": "Name",
+        "email": "Email",
+        "roles": "Roles",
+        "verify": "Verify",
+        "subjectName": "Subject",
+        "description": "Description",
+        "image": "Image",
+        "slug": "Slug",
+        "createTo": "Create By",
+        "like": "Like",
+        "book": "Book",
+        "chapter": "Chapter",
+        "action": {
+            "index": "Actions",
+            "create": "Create",
+            "update": "Update",
+            "delete": "Delete",
+        },
+        "tittle": "Nom Article",
+        "createdAt": "Created At",
+        "updatedAt": "Updated At",
+    },
+    "button": {
+        "edit": "Update",
+        "delete": "Delete",
+        "close": "Close",
+        "editPassword": "Update Password",
+        "deleteAccount": "Delete Account"
+    },
+    "input": {
+        "name": "Name",
+        "email": "Email",
+        "password": "Password",
+        "confirmationPassword": "Confirmation Password",
+    },
+    "title": {
+        "deleteAccount": "Delete Account",
+    },
+    "modal": {
+        "deleteAccount": "Do you really want to delete your account?",
+    }
 }
