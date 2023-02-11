cc0444ec2ea60dc81ef63a41598226507f751eca
tmeunier <theo.meunier41@gmail.com>
2023-02-11 17:21:30 +0100

    clear code edit profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -5,51 +5,44 @@
         </div>
     </x-slot>
 
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 bg-white border-b border-gray-200">
+            <h4>
+                <i class="fa-solid fa-user mr-3"></i>
+                {{ __('page/profile.information') }}
+            </h4>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <h4>
-                        <i class="fa-solid fa-user mr-3"></i>
-                        {{ __('page/profile.information') }}
-                    </h4>
-
-                    <Profile-Edit/>
-                </div>
-            </div>
+            <Profile-Edit/>
         </div>
     </div>
 
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 bg-white border-b border-gray-200">
-                <h4>
-                    <i class="fa-solid fa-lock mr-3"></i>
-                    {{ __('page/profile.password') }}
-                </h4>
 
-                <Profile-Password-Edit/>
-            </div>
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-6">
+        <div class="p-6 bg-white border-b border-gray-200">
+            <h4>
+                <i class="fa-solid fa-lock mr-3"></i>
+                {{ __('page/profile.password') }}
+            </h4>
+
+            <Profile-Password-Edit/>
         </div>
     </div>
 
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-12">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 bg-white border-b border-gray-200">
-                <h4 class="text__danger">
-                    <i class="fa-solid fa-lock mr-3"></i>
-                    {{ __('page/profile.delete.title') }}
-                </h4>
 
-                <div class="mt-3">
-                    <p>{{ __('page/profile.delete.descriptionFirst') }}</p>
-                    <p>{{ __('page/profile.delete.descriptionSeconde') }}</p>
-                </div>
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-6">
+        <div class="p-6 bg-white border-b border-gray-200">
+            <h4 class="text__danger">
+                <i class="fa-solid fa-lock mr-3"></i>
+                {{ __('page/profile.delete.title') }}
+            </h4>
 
-                <div class="flex justify-end">
-                    <Delete-Account/>
-                </div>
+            <div class="mt-3">
+                <p>{{ __('page/profile.delete.descriptionFirst') }}</p>
+                <p>{{ __('page/profile.delete.descriptionSeconde') }}</p>
+            </div>
+
+            <div class="flex justify-end">
+                <Delete-Account/>
             </div>
         </div>
     </div>
