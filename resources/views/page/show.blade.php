1cf704ab49be4195dd17961c4a1305153d3d979e
tmeunier <theo.meunier41@gmail.com>
2023-02-11 14:06:35 +0100

    feat: add button edit page in top

--- resources/views/page/show.blade.php
+++ resources/views/page/show.blade.php
@@ -6,27 +6,19 @@
     </x-slot>
 
     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 bg-white border-b border-gray-200 prose prose-img:rounded-xl prose-a:text-blue-600 max-w-none">
-            <x-markdown :anchors="false">
-                {!! $page->content !!}
-            </x-markdown>
+        <div class="p-6 bg-white border-b border-gray-200">
+            <div class="justify__between">
+                <h1>{{ $page->name }}</h1>
+                <a class="btn btn__primary" href="{{ route('book.chapter.page.edit', ['slug' => $book->slug, 'slugChapter' => $chapter->slug, 'slugPage' => $page->slug]) }}">
+                    <i class="fa-solid fa-pen-to-square mr-2"></i>
+                    {{ __('button.book.edit') }}
+                </a>
+            </div>
 
-            {{-- {!! Illuminate\Support\Str::markdown($page->content) !!}--}}
-        </div>
-    </div>
-
-    <div>
-        <div class="card mt-3 mr-2">
-            <div class="card__body">
-                <h5 class="ms-3">{{ __('title.action') }}</h5>
-                <div class="mt-3">
-                    <div class="my-3">
-                        <a href="{{ route('book.chapter.page.edit', ['slug' => $book->slug, 'slugChapter' => $chapter->slug, 'slugPage' => $page->slug]) }}">
-                            <i class="fa-solid fa-pen-to-square mr-2"></i>
-                            {{ __('button.book.edit') }}
-                        </a>
-                    </div>
-                </div>
+            <div class="prose prose-img:rounded-xl prose-a:text-blue-600 max-w-none">
+                <x-markdown :anchors="false">
+                    {!! $page->content !!}
+                </x-markdown>
             </div>
         </div>
     </div>
