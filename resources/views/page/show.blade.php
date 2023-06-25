66e93e6ed1b38b6b6aacdd07bedeb2ab364e24ff
tmeunier <theo.meunier41@gmail.com>
2023-06-26 00:14:41 +0200

    feat: delete page

--- resources/views/page/show.blade.php
+++ resources/views/page/show.blade.php
@@ -7,20 +7,57 @@
 
     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 bg-white border-b border-gray-200">
-            <div class="justify__between">
-                <h1>{{ $page->name }}</h1>
-                @can('page edit')
-                    <a class="btn btn__primary" href="{{ route('book.chapter.page.edit', ['slug' => $book->slug, 'slugChapter' => $chapter->slug, 'slugPage' => $page->slug]) }}">
-                        <i class="fa-solid fa-pen-to-square mr-2"></i>
-                        {{ __('button.book.edit') }}
-                    </a>
-                @endcan
-            </div>
+            <div class="grid grid-cols-5 gap-4">
+                <div class="flex flex-col">
+                    @canany(['page edit', 'page delete'])
+                        <div class="flex flex-col mt-4 mb-14">
+                            <h4>{{ __('title.action') }}</h4>
+                            @can('page edit')
+                                <a href="{{ route('book.chapter.page.edit', ['slug' => $book->slug, 'slugChapter' => $chapter->slug, 'slugPage' => $page->slug]) }}"
+                                   class="text-blue-500 my-6">
+                                    <i class="fa-solid fa-pen-to-square mr-2"></i>
+                                    {{ __('button.page.edit') }}
+                                </a>
+                            @endcan
+                            @can('page delete')
+                                <form method="POST" action="{{ route('pages.delete', ['slug' => $page->slug]) }}"
+                                      style="display: inline-block"
+                                      onsubmit="return confirm('Etes vous vraiment sur ?')">
+                                    @csrf
+                                    <input type="hidden" name="_method" value="DELETE">
+                                    <button class="text-red-500">
+                                        <i class="fa-solid fa-trash-can mr-2"></i>
+                                        {{ __('button.page.delete') }}
+                                    </button>
+                                </form>
+                            @endcan
+                        </div>
+                    @endcanany
 
-            <div class="min-w-full prose prose-img:rounded-xl prose-a:text-blue-600">
-                <x-markdown>
-                    {!! $page->content !!}
-                </x-markdown>
+                    <div>
+                        <h4>{{ __('title.info') }}</h4>
+                        <p class="text-gray-500 mt-6">
+                            <i class="fa-solid fa-user mr-2"></i>
+                            {{ __('nav.create_to') }} {{ $page->user->name }}
+                        </p>
+                        <p class="text-gray-500 mt-6">
+                            <i class="fa-regular fa-clock mr-2"></i>
+                            {{ __('nav.created_at') }} {{ $page->created_at->format('d/m/Y') }}
+                        </p>
+                    </div>
+                </div>
+
+                <div class="col-span-4">
+                    <div class="pb-9">
+                        <h1>{{ $page->name }}</h1>
+                    </div>
+
+                    <div class="mt-7">
+                        <x-markdown>
+                            {!! $page->content !!}
+                        </x-markdown>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
