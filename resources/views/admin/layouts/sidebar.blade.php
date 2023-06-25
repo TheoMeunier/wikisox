7dbe35902a6c099e013c5ad23a46206cd8709ac7
tmeunier <theo.meunier41@gmail.com>
2023-06-25 14:30:35 +0200

    fix: sidebar actif for children

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -8,19 +8,19 @@
         </li>
     </ul>
     <ul class="sidebar__section">
-        <li class="sidebar__link @if(request()->routeIs('admin.users.index')) sidebar__link__active @endif">
+        <li class="sidebar__link @if(request()->routeIs('admin.users.*')) sidebar__link__active @endif">
             <a href="{{ route('admin.users.index') }}">
                 <i class="fa-solid fa-user"></i>
                 <span>{{  __('nav.users') }}</span>
             </a>
         </li>
-        <li class="sidebar__link @if(request()->routeIs('admin.roles.index')) sidebar__link__active @endif">
+        <li class="sidebar__link @if(request()->routeIs('admin.roles.*')) sidebar__link__active @endif">
             <a href="{{ route('admin.roles.index') }}">
                 <i class="fa-solid fa-user-gear"></i>
                 <span>{{  __('nav.roles') }}</span>
             </a>
         </li>
-        <li class="sidebar__link @if(request()->routeIs('admin.logs.index')) sidebar__link__active @endif">
+        <li class="sidebar__link @if(request()->routeIs('admin.logs.*')) sidebar__link__active @endif">
             <a href="{{ route('admin.logs.index') }}">
                 <i class="fa-solid fa-box-archive"></i>
                 <span>{{  __('nav.logs') }}</span>
@@ -28,19 +28,19 @@
         </li>
     </ul>
     <ul class="sidebar__section">
-        <li class="sidebar__link @if(request()->routeIs('admin.book.index')) sidebar__link__active @endif">
+        <li class="sidebar__link @if(request()->routeIs('admin.book.*')) sidebar__link__active @endif">
             <a href="{{ route('admin.book.index') }}">
                 <i class="fa-solid fa-book-bookmark"></i>
                 <span>{{  __('nav.books') }}</span>
             </a>
         </li>
-        <li class="sidebar__link @if(request()->routeIs('admin.chapters.index')) sidebar__link__active @endif">
+        <li class="sidebar__link @if(request()->routeIs('admin.chapters.*')) sidebar__link__active @endif">
             <a href="{{ route('admin.chapters.index') }}">
                 <i class="fa-solid fa-book-open"></i>
                 <span>{{  __('nav.chapters') }}</span>
             </a>
         </li>
-        <li class="sidebar__link @if(request()->routeIs('admin.pages.index')) sidebar__link__active @endif">
+        <li class="sidebar__link @if(request()->routeIs('admin.pages.*')) sidebar__link__active @endif">
             <a href="{{ route('admin.pages.index') }}">
                 <i class="fa-sharp fa-solid fa-file-circle-check"></i>
                 <span>{{  __('nav.pages') }}</span>
