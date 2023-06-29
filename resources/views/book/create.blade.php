ada98fb677097f780bb7791ab8b9a6e70bc0adba
tmeunier <theo.meunier41@gmail.com>
2023-06-29 21:50:44 +0200

    fix: padding breadcrumb

--- resources/views/book/create.blade.php
+++ resources/views/book/create.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl py-6 text-gray-800 leading-tight">
             {{ Breadcrumbs::render('book.create') }}
         </h2>
     </x-slot>
