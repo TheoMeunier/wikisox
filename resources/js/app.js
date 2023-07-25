15c4fe81b500a93ba7c980a0daf12d5a425a21f8
tmeunier <theo.meunier41@gmail.com>
2023-07-25 23:18:59 +0200

    fix: clear code

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,8 @@ import { createApp } from 'vue'
 import Books from './components/books.vue'
 import Chapters from './components/chapters.vue'
 import Pages from './components/pages.vue'
-import ProfileEdit from './components/profile/profileEdit.vue'
-import ProfilePasswordEdit from './components/profile/profilePasswordEdit.vue'
 import AdminImages from './components/admin/AdminImages.vue'
 import { createPinia } from 'pinia'
-import ProfileLog from "./components/profile/profileLog.vue";
 
 const pinia = createPinia()
 const app = createApp(undefined, undefined)
@@ -18,9 +15,6 @@ app.use(pinia)
 app.component('Books', Books)
 app.component('Chapters', Chapters)
 app.component('Pages', Pages)
-app.component('profileEdit', ProfileEdit)
-app.component('profilePasswordEdit', ProfilePasswordEdit)
-app.component('profileLog', ProfileLog)
 app.component('adminImages', AdminImages)
 
 app.mount('#app')
