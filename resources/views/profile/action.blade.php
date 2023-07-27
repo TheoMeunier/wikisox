5da84831260302db275d281b72b208fb15abd227
tmeunier <theo.meunier41@gmail.com>
2023-07-27 23:15:57 +0200

    fix: margin top profile

--- resources/views/profile/action.blade.php
+++ resources/views/profile/action.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.profile.layouts>
-    <section class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-6">
+    <section class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-3">
         <div class="p-6 bg-white border-b border-gray-200">
             @livewire('profile.profile-log-activity-livewire')
         </div>
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.profile.layouts>
-    <section class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+    <section class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-3">
         <div class="p-6 bg-white border-b border-gray-200">
             <h1 class="h4 mb-3">
                 <i class="fa-solid fa-user mr-3 text-indigo-500"></i>
