f968a85d8790bf9a4553fe59e88b4b6359a6f053
tmeunier <theo.meunier41@gmail.com>
2023-10-21 12:03:06 +0200

    fix: design page admin edit

--- resources/views/admin/books/edit.blade.php
+++ resources/views/admin/books/edit.blade.php
@@ -10,38 +10,36 @@
         <form action="{{ route('admin.book.update', ['slug' => $book->slug]) }}" method="POST">
             @csrf
 
-            <div class="flex gap-5">
-                <div class="w-3/4">
-                    <div>
-                        <x-label for="name" :value="__('input.label.name')"/>
-                        <x-input id="name" class="block mt-1 w-full" type="text" name="name" value="{{ $book->name }}"
-                                 required autofocus/>
-                        @error('name')
-                        <div class="text-red-500">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="mt-10">
-                        <x-label for="description" :value="__('input.label.description')"/>
-                        <x-forms.textarea name="description" id="description"
-                                          value="{{ $book->description }}"></x-forms.textarea>
-                        @error('description')
-                        <div class="text-red-500">{{ $message }}</div>
-                        @enderror
-                    </div>
+            <div>
+                <div>
+                    <x-label for="name" :value="__('input.label.name')"/>
+                    <x-input id="name" class="block mt-1 w-full" type="text" name="name" value="{{ $book->name }}"
+                             required autofocus/>
+                    @error('name')
+                    <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
-                <div class="w-2/4">
+                <div class="w-1/4 mt-4">
                     <x-label for="image" :value="__('input.label.image')"/>
                     <x-forms.image :name="'image'" value="{{ $book->image }}"/>
                     @error('image')
                     <div class="text-red-500">{{ $message }}</div>
                     @enderror
                 </div>
+                <div class="mt-4">
+                    <x-label for="description" :value="__('input.label.description')"/>
+                    <x-forms.textarea name="description" id="description"
+                                      value="{{ $book->description }}"></x-forms.textarea>
+                    @error('description')
+                    <div class="text-red-500">{{ $message }}</div>
+                    @enderror
+                </div>
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <x-links.link-button-primary href="{{ url()->previous() }}">
+                <x-links.link-button-secondary href="{{ url()->previous() }}">
                     {{ __('button.action.close') }}
-                </x-links.link-button-primary>
+                </x-links.link-button-secondary>
                 <x-button class="ml-3">
                     {{ __('button.action.edit') }}
                 </x-button>
