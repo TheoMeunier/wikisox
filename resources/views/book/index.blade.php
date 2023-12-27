f14e3cb206762eb99d2144a1851f9e29bfffeb7c
tmeunier <theo.meunier41@gmail.com>
2023-12-27 13:50:46 +0100

    fix: button create book

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -1,7 +1,13 @@
 <x-app-layout>
     <section class="container my-5">
-        <div>
+        <div class="justify__between">
             <h1>{{ __('title.books') }}</h1>
+            @can('book create')
+                <x-links.link-button-primary href="{{ route('book.create') }}">
+                    <x-icons.icon-plus class="w-4 h-4"/>
+                    {{ __('button.book.create') }}
+                </x-links.link-button-primary>
+            @endcan
         </div>
 
         @livewire('book-livewire')
