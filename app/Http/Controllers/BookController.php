935517cb06047860103658557e51724d0b0a9601
tmeunier <theo.meunier41@gmail.com>
2024-03-21 18:02:51 +0100

    fix: clear code

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\BookRequest;
 use App\Models\Book;
-use App\Services\FileSystem\FileSystemService;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
