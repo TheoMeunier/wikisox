73a1efc806c5583fd0d7dec98741a20b7e85aeb7
tmeunier <theo.meunier41@gmail.com>
2024-04-06 22:53:13 +0200

    fix: try catch for glide

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -7,24 +7,29 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use League\Glide\Responses\LaravelResponseFactory;
 use League\Glide\ServerFactory;
+use League\Glide\Signatures\SignatureException;
 use League\Glide\Signatures\SignatureFactory;
 
 class ImageController extends Controller
 {
     public function show(Request $request, string $path): mixed
     {
-        SignatureFactory::create(config('glide.key'))->validateRequest($request->path(), $request->all());
-        $driver = GlideImageService::getDriver();
+        try {
+            SignatureFactory::create(config('glide.key'))->validateRequest($request->path(), $request->all());
+            $driver = GlideImageService::getDriver();
 
-        $server = ServerFactory::create([
-            'source'            => $driver,
-            'cache'             => Storage::disk('local')->getDriver(),
-            'driver'            => 'imagick',
-            'cache_path_prefix' => '.cache',
-            'response'          => new LaravelResponseFactory(),
-            'base_url'          => 'images',
-        ]);
+            $server = ServerFactory::create([
+                'source'            => $driver,
+                'cache'             => Storage::disk('local')->getDriver(),
+                'driver'            => 'imagick',
+                'cache_path_prefix' => '.cache',
+                'response'          => new LaravelResponseFactory(),
+                'base_url'          => 'images',
+            ]);
 
-        return $server->getImageResponse($path, $request->all());
+            return $server->getImageResponse($path, $request->all());
+        } catch (SignatureException $e) {
+            abort(403);
+        }
     }
 }
