3ee47d802a27325040f3016a27e25a02124b0ee4
tmeunier <theo.meunier41@gmail.com>
2024-04-06 22:36:09 +0200

    fix: button copy for code

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,11 +2,39 @@ import './libs/alphinejs'
 import './libs/file-manager'
 import './libs/markdown-editor'
 import hljs from 'highlight.js'
-import CopyButtonPlugin from 'highlightjs-copy'
 import axios from 'axios'
 
-hljs.addPlugin(new CopyButtonPlugin())
 hljs.highlightAll()
+
+// copy code snippet
+let snippets = document.getElementsByTagName('pre');
+let numberOfSnippets = snippets.length;
+
+if (snippets) {
+    for (let i = 0; i < numberOfSnippets; i++) {
+        let code = snippets[i].getElementsByTagName('code')[0].innerText;
+        snippets[i].classList.add('hljs'); // append copy button to pre tag
+        snippets[i].innerHTML = '<button class="hljs-copy">Copy</button>' + snippets[i].innerHTML; // append copy button
+
+        snippets[i].getElementsByClassName('hljs-copy')[0].addEventListener("click", function () {
+            this.innerText = 'Copying..';
+            if (!navigator.userAgent.toLowerCase().includes('safari')) {
+                navigator.clipboard.writeText(code);
+            } else {
+                prompt("Clipboard (Select: ⌘+a > Copy:⌘+c)", code);
+            }
+
+            this.innerText = 'Copied!';
+
+            let button = this;
+            setTimeout(function () {
+                button.innerText = 'Copy';
+            }, 1000)
+
+        });
+
+    }
+}
 
 //avatar
 let avatar_input = document.querySelector('#form__avatar input')
